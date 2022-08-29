f5a647d686ac0617f1e1f10f04b6e31fdb294128
Ros <Fedorovch>
2022-08-29 16:18:36 +0300

    Add helper 'currently_at'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,19 @@
 module ApplicationHelper
-  def currently_at()
-    render partial: 'shared/menu'
+  def nav_tab(title, url, options = {})
+    current_page = options.delete :current_page
+
+    css_class = current_page == title ? 'text-secondary' : 'text-white' 
+
+    options[:class] = if options[:class]
+                        options[:class] + ' ' + css_class 
+                      else
+                        css_class
+                      end
+        
+    link_to title, url, options
+  end
+  def currently_at(current_page = '')
+    render partial: 'shared/menu', locals: {current_page: current_page}
   end
 
   def full_title(page_title = "")
