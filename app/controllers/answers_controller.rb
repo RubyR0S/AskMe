e7961fbefc4575116ff14a70ed4529237189e8f0
Ros <Fedorovch>
2022-08-30 16:23:32 +0300

    Anchor update

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,5 @@
 class AnswersController < ApplicationController
+  include ActionView::RecordIdentifier
   before_action :find_question!
   before_action :find_answer!, except: :create
 
@@ -9,7 +10,7 @@ class AnswersController < ApplicationController
   def update
     if @answer.update answer_params
       flash[:success] = "Answer updated!"
-      redirect_to question_path(@question, anchor: "answer-#{@answer.id}")
+      redirect_to question_path(@question, anchor: dom_id(@answer))
     else
       render :edit
     end
