27e6bed37ebbe386cdae56a08434dbf24c249f85
Ros <Fedorovch>
2022-08-31 15:06:12 +0300

    Create model User and users_controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_26_131522) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_171832) do
   create_table "answers", force: :cascade do |t|
     t.text "body"
     t.integer "question_id", null: false
@@ -24,6 +24,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_26_131522) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "answers", "questions"
