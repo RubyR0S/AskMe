35aef90abb0b9d69c0f14ffa060f89e9229f7f44
Ros <Fedorovch>
2022-09-03 20:48:17 +0300

    Creating sessions_controller and sesions routes

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by email: params[:email]
+    if user&.authenticate(params[:password])
+      sign_in user
+      flash[:success] = "Welcome back, #{current_user.name_or_email}"
+      redirect_to root_path
+    else
+      flash[:warning] = "Incorrect email and/or password!"
+      redirect_to new_session_path
+    end
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+resources :sessions, only: %i[new create destroy]
+
 resources :users, only: %i[new create]
 
 resources :questions do
