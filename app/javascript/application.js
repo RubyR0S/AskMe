938e055eadd5431c1a9a2b9f13adb0f0c6a0e0a5
Ros <Fedorovch>
2022-09-03 20:51:16 +0300

    Added 'dropdown'

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,9 +3,14 @@
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+
+
+import '@popperjs/core'
+import 'bootstrap/js/dist/dropdown' 
 
 Rails.start()
 Turbolinks.start()
-// bootstrap.start()
+// ActiveStorage.start()
 
-import * as bootstrap from "bootstrap"
