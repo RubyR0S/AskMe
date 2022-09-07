6d5232a1012412797426812308bd5e5703196d6f
Ros <Fedorovch>
2022-09-07 14:42:49 +0300

    Adding a cookie

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -1,26 +1,45 @@
-module Authentication 
+module Authentication
   extend ActiveSupport::Concern
 
+  # rubocop:disable Metrics/BlockLength
   included do
     private
 
     def current_user
-      @current_user ||= User.find_by(id: session[:user_id]).decorate if session[:user_id].present?
+      user = session[:user_id].present? ? user_from_session : user_from_token
+
+      @current_user ||= user&.decorate
     end
-  
+
+    def user_from_session
+      User.find_by(id: session[:user_id])
+    end
+
+    def user_from_token
+      user = User.find_by(id: cookies.encrypted[:user_id])
+      token = cookies.encrypted[:remember_token]
+
+      return unless user&.remember_token_authenticated?(token)
+
+      sign_in user
+      user
+    end
+
     def user_signed_in?
       current_user.present?
     end
 
-    def require_no_authentication
-      return if !user_signed_in?
-      flash[:warning] = "You are already signed in!"
+    def require_authentication
+      return if user_signed_in?
+
+      flash[:warning] = 'You are not signed in!'
       redirect_to root_path
     end
 
-    def require_authentication
-      return if user_signed_in?
-      flash[:warning] = "You are not signed in!"
+    def require_no_authentication
+      return unless user_signed_in?
+
+      flash[:warning] = 'You are already signed in!'
       redirect_to root_path
     end
 
@@ -28,10 +47,25 @@ module Authentication
       session[:user_id] = user.id
     end
 
+    def remember(user)
+      user.remember_me
+      cookies.encrypted.permanent[:remember_token] = user.remember_token
+      cookies.encrypted.permanent[:user_id] = user.id
+    end
+
+    def forget(user)
+      user.forget_me
+      cookies.delete :user_id
+      cookies.delete :remember_token
+    end
+
     def sign_out
+      forget current_user
       session.delete :user_id
+      @current_user = nil
     end
 
     helper_method :current_user, :user_signed_in?
   end
+  # rubocop:enable Metrics/BlockLength
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,11 +9,12 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:email]
     if user&.authenticate(params[:password])
       sign_in user
+      remember(user) if params[:remember_me] == '1'
       flash[:success] = "Welcome back, #{current_user.name_or_email}"
       redirect_to root_path
     else
       flash[:warning] = "Incorrect email and/or password!"
-      redirect_to new_session_path
+      render :new
     end
   end
 
